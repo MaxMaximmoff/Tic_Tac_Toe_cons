4d3590f5f617d030f6a107aa0127cefe88001246
MaxMaximmoff <MioMan1997@gmail.com>
2022-03-16 14:54:34 +0300

    убрал комментариии в Main

--- src/com/maximoff/Main.java
+++ src/com/maximoff/Main.java
@@ -7,23 +7,11 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-//        GameController gameController = new GameController(
-//                                             new GameModel(), new GameView());
-//
-//        gameController.play();
-        int[] pos = new int[]{0, 0};
-        int pos_int = 9;
-        int i = 1;
-        for (int row=0; row<3; row++) {
-            for (int col=0; col<3; col++) {
-                if (i==pos_int) {
-                    pos[1] = row;
-                    pos[0] = col;
-                }
-                i++;
-            }
-        }
-        System.out.println("col=" + pos[0] + " row=" + pos[1] );
+        GameController gameController = new GameController(
+                                             new GameModel(), new GameView());
+
+        gameController.play();
+
     }
 }
 
